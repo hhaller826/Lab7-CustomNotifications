e72c8d6f4f26e4735b375a449c996e7261b9df23
hanita <hhaller@wisc.edu>
2021-11-14 19:31:02 -0600

    Milestone 1

---
+++ app/src/main/java/com/example/lab7customnotifications/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.lab7customnotifications;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
+    }
+
+    public void sendOnChannel1(View v) {
+        editTextTitle = (EditText) findViewById(R.id.title);
+        editTextMessage = (EditText) findViewById(R.id.message);
+
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_baseline_tag_faces_24)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
+
+    public void sendOnChannel2(View v) {
+        editTextTitle = (EditText) findViewById(R.id.title);
+        editTextMessage = (EditText) findViewById(R.id.message);
+
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_baseline_free_breakfast_24)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(2, notification);
+    }
+}
+
